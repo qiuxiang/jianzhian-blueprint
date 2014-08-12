72e9ab4a5d3239271387226b2d8abf2355babf86
Qiu Xiang <xiang.qiu@qq.com>
2014-08-12 11:29:02 +0800

    directives 不再接受属性传值，而是取控制器的 $scope

--- assets/scripts/directives.js
+++ assets/scripts/directives.js
@@ -2,36 +2,27 @@ angular.module('directives', [])
   .directive('blueprintFoundation', function () {
     return {
       restrict: 'E',
-      scope: {
-        width: '@',
-        height: '@',
-        scale: '@'
-      },
       templateUrl: 'views/directives/blueprints/foundation.html',
       controller: ['$scope', function ($scope) {
-        $scope.$watch('width', calculate)
-        $scope.$watch('height', calculate)
-        $scope.$watch('scale', calculate)
+        $scope.$watch('metadata', function () {
+          $scope.base_side = parseInt(1820 * $scope.metadata.scale)
+          $scope.base_width = $scope.metadata.width * $scope.base_side
+          $scope.base_height = $scope.metadata.height * $scope.base_side
 
-        function calculate() {
-          $scope.base_side = parseInt(1820 * $scope.scale)
-          $scope.base_width = $scope.width * $scope.base_side
-          $scope.base_height = $scope.height * $scope.base_side
-
-          $scope.outer_diff = parseInt(380 * $scope.scale)
+          $scope.outer_diff = parseInt(380 * $scope.metadata.scale)
           $scope.outer_width = $scope.base_width + $scope.outer_diff
           $scope.outer_height = $scope.base_height + $scope.outer_diff
 
-          $scope.center_diff = parseInt(80 * $scope.scale)
+          $scope.center_diff = parseInt(80 * $scope.metadata.scale)
           $scope.center_offset = parseInt(($scope.outer_diff - $scope.center_diff) / 2)
           $scope.center_width = $scope.base_width + $scope.center_diff
           $scope.center_height = $scope.base_height + $scope.center_diff
 
-          $scope.inner_diff = parseInt(-220 * $scope.scale)
+          $scope.inner_diff = parseInt(-220 * $scope.metadata.scale)
           $scope.inner_offset = parseInt(($scope.outer_diff - $scope.inner_diff) / 2)
           $scope.inner_width = $scope.base_width + $scope.inner_diff
           $scope.inner_height = $scope.base_height + $scope.inner_diff
-        }
+        })
       }]
     }
   })
